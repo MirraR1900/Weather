6c38f1a2af13c5e766744091f978d0e85d8943b3
Mirra R <mirrarichter@gmail.com>
2021-09-05 03:13:17 +0300

    commit

--- src/components/BlockWeatherWeek.tsx
+++ src/components/BlockWeatherWeek.tsx
@@ -1,29 +1,47 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { WeatherDay } from '../components/WeatherDay';
 
 type BlockWeatherWeek = {
-    arrayWeather: any
+  arrayWeather: any
+}
+
+export const BlockWeatherWeek: React.FC<BlockWeatherWeek> = ({ arrayWeather }) => {
+
+  const [indexstep, setStep] = useState(0);
+  const classLeft: any = [];
+  const classRight: any = [];
+  const width = document.documentElement.clientWidth;
+
+  if (indexstep == 0) {
+    classRight.push("arrowsOpasity");
+  } else if ((indexstep == 4 && width > 650) || (indexstep == 6 && width < 650)) {
+    classLeft.push("arrowsOpasity");
   }
 
-export const BlockWeatherWeek: React.FC<BlockWeatherWeek> = ({arrayWeather}) => {
+  const clickArrow = (e: any) => {
+    let target = e.target;
+    if (indexstep < arrayWeather.length - 1 && target.id === "left") {
+      setStep(indexstep + 1)
+    } else if (indexstep >= 0 && target.id === "right") {
+      setStep(indexstep - 1)
+    }
+  }
 
-    const [indexstep, setStep] = useState(0);
-
-    return(
-        <div className="container">
-        <div className="arrows"><img src="images/arrowLeft.png" onClick={() => { if (indexstep < arrayWeather.length - 1) setStep(indexstep + 1) }} /></div>
-        <div className="containerWeather">
-          {
-            arrayWeather.map((item: any, index: number) => {
-              if (index >= indexstep) {
-                return (
-                  <WeatherDay key={index} date={item.day} temp={item.temp} weather={item.weather} />
-                )
-              }
-            })
-          }
-        </div>
-        <div className="arrows"><img src="images/arrowRight.png" onClick={() => { if (indexstep >= 0) setStep(indexstep - 1) }} /></div>
+  return (
+    <div className="container">
+      <div className="arrows"><img id="left" className={classLeft.join(' ')} src="images/arrowLeft.png" onClick={clickArrow} /></div>
+      <div className="containerWeather">
+        {
+          arrayWeather.map((item: any, index: number) => {
+            if (index >= indexstep) {
+              return (
+                <WeatherDay key={index} date={item.day} temp={item.temp} weather={item.weather} />
+              )
+            }
+          })
+        }
       </div>
-    )
+      <div className="arrows"><img id="right" className={classRight.join(' ')} src="images/arrowRight.png" onClick={clickArrow} /></div>
+    </div>
+  )
 }
